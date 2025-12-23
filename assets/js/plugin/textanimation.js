bf58896b66a0744f94aac16813942912d5e0fdf8
Suraj Banerjee <surajbanerjee255@gmail.com>
2025-12-23 13:25:36 +0530

    Redesign Contact Us page and update branding
    Revamped Contact-us.html with new branding, updated meta tags, navigation, and content structure. Added new favicon assets, improved about and intro sections, and enhanced visual presentation. Updated styles and images to reflect the WebTrion brand and modernized the user experience.

--- assets/js/plugin/textanimation.js
+++ assets/js/plugin/textanimation.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function ($) {
     //set animation timing
-    var animationDelay = 2500,
+    var animationDelay = 3000,
         //loading bar effect
         barAnimationDelay = 3800,
         barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
         //letters effect
         lettersDelay = 50,
         //type effect
-        typeLettersDelay = 150,
+        typeLettersDelay = 500,
         selectionDuration = 500,
         typeAnimationDelay = selectionDuration + 800,
         //clip effect 
